391e6de342eb8e1235455107b276f667de51532c
Fazal Hamza Khan <theunknown.pak@gmail.com>
2025-01-06 21:33:18 +0500

    Some Work

--- lib/Screens/bodycode.dart
+++ lib/Screens/bodycode.dart
@@ -10,12 +10,37 @@ class BodyCode extends StatefulWidget {
 class _BodyCodeState extends State<BodyCode> {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Container(
-        color: Theme.of(context).primaryColor,
-        height: 80,
-        width: 80,
-      ),
+    return ListView.builder(
+      scrollDirection: Axis.horizontal,
+      itemCount: 5,
+      itemBuilder: (BuildContext context, int index) {
+        return Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 2),
+          child: Column(
+            children: [
+              Container(
+                height: 50, // Set the height of the rectangle
+                width: 100, // Set the width of the rectangle
+                decoration: BoxDecoration(
+                  color: Colors.yellow[500], // Highlight color
+                  borderRadius: BorderRadius.circular(8), // Rounded corners
+                  border: Border.all(
+                    color: Colors.black, // Border color (optional)
+                    width: .5, // Border width (optional)
+                  ),
+                ),
+                child: Center(
+                    child: Text(
+                  "Green",
+                  style: TextStyle(
+                      color: Theme.of(context).textTheme.headlineSmall?.color,fontSize: 20),
+
+                )),
+              ),
+            ],
+          ),
+        );
+      },
     );
   }
 }
